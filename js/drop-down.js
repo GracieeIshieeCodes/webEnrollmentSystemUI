6ce2805726c07d87ad3959b8df13f8d63d909a7f
GracieeIshieeCodes <gracieeishiee@gmail.com>
2024-08-02 13:35:25 +0800

    update js dropdown

--- js/drop-down.js
+++ js/drop-down.js
@@ -1,25 +1,27 @@
 $(document).ready(function(){
-    $('.nav-item.dropdown').hover(
-        function () {
+    if (!('ontouchstart' in window)) {  // Check if the device is not a touch device
+        $('.nav-item.dropdown').hover(
+            function () {
+                var $dropdownMenu = $(this).find('.dropdown-menu').first();
+                $dropdownMenu.stop(true, true).slideDown(300).addClass('show');
+            },
+            function () {
+                var $dropdownMenu = $(this).find('.dropdown-menu').first();
+                $dropdownMenu.stop(true, true).slideUp(200, function() {
+                    $(this).removeClass('show');
+                });
+            }
+        );
+        
+        // For keyboard accessibility
+        $('.nav-item.dropdown').focusin(function() {
             var $dropdownMenu = $(this).find('.dropdown-menu').first();
             $dropdownMenu.stop(true, true).slideDown(300).addClass('show');
-        },
-        function () {
+        }).focusout(function() {
             var $dropdownMenu = $(this).find('.dropdown-menu').first();
             $dropdownMenu.stop(true, true).slideUp(200, function() {
                 $(this).removeClass('show');
             });
-        }
-    );
-    
-    // For keyboard accessibility
-    $('.nav-item.dropdown').focusin(function() {
-        var $dropdownMenu = $(this).find('.dropdown-menu').first();
-        $dropdownMenu.stop(true, true).slideDown(300).addClass('show');
-    }).focusout(function() {
-        var $dropdownMenu = $(this).find('.dropdown-menu').first();
-        $dropdownMenu.stop(true, true).slideUp(200, function() {
-            $(this).removeClass('show');
         });
-    });
+    }
 });
